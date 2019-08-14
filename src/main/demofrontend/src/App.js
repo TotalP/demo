e47d2b24c587d3f79a3754651d7a233c6f90aa74
pavlo.fandych <pavlo.fandych@avid.com>
2019-08-14 18:45:47 +0300

    investigation.

--- src/main/demofrontend/src/App.js
+++ src/main/demofrontend/src/App.js
@@ -7,7 +7,6 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <p>Welcome!</p>
                 <a className="App-link" href="https://ru.reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
             </header>
         </div>
